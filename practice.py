460962b97f8835f7646a7120f8f0f82b9240838d
huyeet <hnguyen21@pickeringcollege.on.ca>
2021-01-04 09:27:12 -0500

    this is gonna be bad for me.

--- practice.py
+++ practice.py
@@ -16,11 +16,18 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill((0, 255, 255))
+    wat = 0
+    times = 50
     #pygame.draw.circle(screen, (255, 0, 0), (300, 50), 20)
-    pygame.draw.line(screen, (255, 0, 0), (width / 2, 0), (width / 2, height))
-    pygame.draw.line(screen, (255, 0, 0), (0, height / 2), (width, height / 2))
-    pygame.draw.line(screen, (0, 100, 0), (0, 0), (width, height))
-    pygame.draw.line(screen, (0, 100, 0), (width, 0), (0, height))
+    # 1024, 1 => 1023, 2 => 1022, 3
+    for i in range(width):
+        pygame.draw.line(screen, (255, 0, 0), (width - i, 0), (0, wat + 1))
+        wat += 1
+
+    #pygame.draw.line(screen, (255, 0, 0), (width / 2, 0), (width / 2, height))
+    #pygame.draw.line(screen, (255, 0, 0), (0, height / 2), (width, height / 2))
+    #pygame.draw.line(screen, (0, 100, 0), (0, 0), (width, height))
+    #pygame.draw.line(screen, (0, 100, 0), (width, 0), (0, height))
     #pygame.draw.polygon(screen, (255, 0, 0), ((146, 0), (291, 186), (236, 277), (56, 277), (0, 186)))
     # Buffering the drawing to reduce flickers
     pygame.display.flip()
